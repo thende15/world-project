ce4282dc7d9350e54d2aaffd36c11082d7b9c6a5
Timothy Henderson <titansmith8@hotmail.com>
2022-10-13 13:33:10 -0400

    route module and service set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildCountriesComponent } from './components/child-countries/child-countries.component';
+import { HomeMainComponent } from './components/home-main/home-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/world', pathMatch: 'full'}, 
+  {path: 'world', component: HomeMainComponent}, 
+  {path: ':name', component: ChildCountriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/services/world.service.ts
+++ src/app/services/world.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorldService {
+  url = "https://restcountries.com/v3.1/all"
 
-  constructor() { }
+  getData(): Observable<any> {
+    return this.http.get(this.url)
+  }
+  constructor(private http: HttpClient) { }
 }
