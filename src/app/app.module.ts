93ea5558e948473f001f8057c221e462064a842a
Timothy Henderson <titansmith8@hotmail.com>
2022-10-14 15:43:19 -0400

    2 pipes, 1 finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeMainComponent } from './components/home-main/home-main.component';
 import { ChildCountriesComponent } from './components/child-countries/child-countries.component';
+import { SearchPipePipe } from './pipes/search-pipe.pipe';
+import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeMainComponent,
-    ChildCountriesComponent
+    ChildCountriesComponent,
+    SearchPipePipe,
+    FilterPipePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/filter-pipe.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { CountriesInterface } from '../interfaces/countries-interface'
+
+@Pipe({
+  name: 'filterPipe'
+})
+export class FilterPipePipe implements PipeTransform {
+
+  transform(countries: CountriesInterface[], filterContent: string, name: string): CountriesInterface[] {
+    if (!filterContent) {
+      return countries;
+    }
+    let filteredContent: CountriesInterface[] = [];
+    for (let country of countries) {
+      if (country.name.official.toLowerCase().includes(filterContent.toLowerCase())) {
+        filteredContent.push(country);
+      }
+    }
+    return filteredContent;
+  }
+
+}
